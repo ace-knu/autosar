c36007eae897121fd9f03b4d429b9877058bca2c
jcho-knu <jcho@knu.ac.kr>
2022-04-08 14:16:06 +0900

    Delete mpc5606b/exercises/backup_Base directory

--- mpc5606b/exercises/backup_Base/Build/remove_excluded.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd %0\..\..
-setlocal
-set PYTHON_HOME="D:\ts_mirr\Python\Portable Python 2.7.5.1\App"
-set PATH=%PYTHON_HOME%
-%PYTHON_HOME%\python .\Build\site_scons\cdt\remove_excluded.py Debug 
-endlocal
-popd
-::pause
